4b8da4b97a0c5d4dc40047cd49e6b72d5c90aea5
7anurag7 <f2013693@goa.bits-pilani.ac.in>
2014-11-09 14:25:41 +0530

    Adding j-unit test for Person class

--- src/zephyr/j-unit tests/TestPerson.java
+++ src/zephyr/j-unit tests/TestPerson.java
@@ -2,27 +2,27 @@ import static org.junit.Assert.assertEquals ;
 import org.junit.Test ; 
 import java.sql.* ;
 import java.lang.* ; 
-public class TestCompetition
+public class TestPerson
 {
 	@Test
-		public void testgetPeople()
+		public void testgetCompetitions()
 		{
 			boolean thrown1 = false;
 			boolean thrown2 = false;
 			Statement stmt = null ;
 			int ID = 1234;																	//random value assigned for checking
 			Person p = new Person(ID,"Sumit","Khaitan","CH5");								//creating an instance of person with ID = 1234
-			Competition cmp = new Competition ( "football", "lt-3", 2015, 7, 17, ID, 0);		//registering a person for competition with ID = 1234
-			ArrayList<Person> al1= new ArrayList<Person>();
+			Competition cmp = new Competition ( "football", "lt-3", 2015, 7, 17, ID, 0);	//registering a person for competition with ID = 1234
+			ArrayList<Competition> al1= new ArrayList<Competition>();
 			try
 			{
 				Class.forName("org.sqlite.JDBC");
 				Connection c = DriverManager.getConnection("jdbc:sqlite:common");
 				stmt = c.createStatement() ; 
 				
-				al1 = cmp.getPeople(stmt);													//getting the list of the persons in the competition
+				al1 = p.getCompetitions(stmt);												//getting the list of the competitions for the person
 				
-				if(al1.contains(p))															//checking the list for the given ID
+				if(al1.contains(cmp))														//checking the list for the given competition
 					assertTrue(true);
 				else
 					fail("Person with the id was not detected");
