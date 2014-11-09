5fb508911706d8e90fbed207b1806951d4c5a275
7anurag7 <f2013693@goa.bits-pilani.ac.in>
2014-11-09 14:44:17 +0530

    Added Test Suit and updated Runner class

--- src/zephyr/j-unit tests/TestRunner.java
+++ src/zephyr/j-unit tests/TestRunner.java
@@ -2,11 +2,12 @@ import org.junit.runner.* ;
 import org.junit.Test ; 
 import org.junit.runner.JUnitCore ;
 import org.junit.runner.notification.Failure ; 
-public class TestRunner 
-{
-	public static void main(String[] args)
-	{
-		Result result = JUnitCore.runClasses(TestUtility.class) ; 
+import org.junit.runner.Result;
+
+public class TestRunner {
+
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(JunitTestSuite.class);
 		for(Failure fail : result.getFailures() )	
 		{
 			System.out.println(fail.toString()) ; 
