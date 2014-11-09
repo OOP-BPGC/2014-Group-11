9fdcc07539317e255ec429151caaac8aa47bef77
R Srinath <f2012560@goa.bits-pilani.ac.in>
2014-11-09 13:07:29 +0530

    Replaced Person class with Student class - better naming convention.

--- src/zephyr/Student.java
+++ src/zephyr/Student.java
@@ -1,67 +1,87 @@
-package zephyr;
-
-import java.util.* ;
-import zephyr.util.*;
-
+package zephyr ; 
+import java.util.* ; 
+import zephyr.* ;  
+import java.sql.* ;
 public class Student implements DatabaseEntry
 {
-    private String fName ; 
-    private String lName ; 
-    private String hostel ; 
-    private String id ; 
-    private String password ;
-    
-    public Student(String id, String fn, String ln, String host)
-    {
-        fName = fn ; 
-        lName = ln ; 
-        hostel = host ; 
-    }
-
-    public void putToDatabase() {
-
-    }
-    public void setFName(String fn)
-    {
-        this.fName = fn ; 
-    }
-    public void setLName(String ln)
-    {
-        this.lName = ln ; 
-    }
-    public void setHostel(String host)
-    {
-        hostel = host ; 
-    }
-    public String getFName()
-    {
-        return fName ; 
-    }
-    public String getLName()
-    {
-        return lName ; 
-    }
-    public String getHostel()
-    {
-        return hostel ; 
-    }
-
-    public ArrayList<TShirt> getTShirts()
-    {
-        /* Query DataBase. */
-    }
-    public ArrayList<Passes> getPasses()
-    {
-        /* Query */ 
-    }
-    public void poll(Event event, int choice) 
-    {
-        ; 
-    } 
-    public ArrayList<Pair> getPerformance()
-    {
-        /* We can use HashMaps also. */ 
-    }	
-    // TODO: - Write interfaces for TShirts and Passes. 
+	private String fName ; 
+	private String lName ; 
+	private String hostel ; 
+	private int  id ; 
+	private String password ; 
+	public Student(int i, String fn, String ln, String host)
+	{
+		id = i ; 
+		fName = fn ; 
+		lName = ln ; 
+		hostel = host ; 
+	}
+	public void setPassword(String pass)
+	{
+		password = pass ; 
+	}
+	public void setFName(String fn)
+	{
+		this.fName = fn ; 
+	}
+	public void setLName(String ln)
+	{
+		this.lName = ln ; 
+	}
+	public void setHostel(String host)
+	{
+		hostel = host ; 
+	}
+	public String getPassword()
+	{
+		return password ; 
+	}
+	public String getFName()
+	{
+		return fName ; 
+	}
+	public String getLName()
+	{
+		return lName ; 
+	}
+	public String getHostel()
+	{
+		return hostel ; 
+	}
+	//public ArrayList<TShirt> getTShirts()
+	//{
+	//	/* Query DataBase. */
+	//}
+	//public ArrayList<Pass> getPasses()
+	//{Spelling of mentees
+	//	/* Query */ 
+	//}
+	//public void castoll(Event event, int choice) 
+	//{
+	//	; 
+	//}
+	//public ArrayList<Pair> getPerformance()
+	//{
+	//	/* We can use HashMaps also. */ 
+	//}	
+	/* Writes itself to the database. */ 
+	public ArrayList<Competition> getCompetitions(Statement stmt) throws SQLException 
+	{
+		String sql = "SELECT * FROM Competition WHERE id=" + id ; 
+		ResultSet rs = stmt.executeQuery(sql) ; 
+		ArrayList<Competition> list = new ArrayList<Competition>() ; 
+		while(rs.next() == true)
+		{
+			list.add(new Competition(rs.getString("name"), rs.getString("venue"), rs.getInt("year"), rs.getInt("month"), rs.getInt("date"), id, rs.getInt("rank"))) ;
+		}
+		return list ; 
+	} 	
+	public void writeToDataBase(Statement stmt) throws SQLException 
+	{
+		String sql = "INSERT INTO Person (fname, lname, hostel, id, password, type) VALUES ('" + fName + "','" +lName + "','"+hostel + "',"+id+",'"+password+"',"+0 + ");" ;
+		stmt.executeUpdate(sql) ; 
+		;	
+	}
+	// TODO: - Write interfaces for TShirts and Passes. 
 	
 }
