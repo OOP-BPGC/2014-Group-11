fc8f3661c096e6eb817500530b91c70135c65fd8
ps06756 <ps06756@gmail.com>
2014-11-14 04:53:47 +0530

    Working code with maven and makefile

---
+++ zephyr/src/main/java/com/group11/app/classes/Event.java
@@ -0,0 +1,41 @@
+
+package classes ; 
+import java.util.* ;
+
+public abstract class Event
+{
+	public String venue; 	
+	public int year ; 
+	public int month ; 
+	public int date ; 
+	public String name ; 
+	//abstract ArrayList<Person> getPeople() ;
+
+	//abstract ArrayList<Student> getStudent();
+    
+	public int getYear()
+	{
+		return year ; 
+	}
+	public int getMonth()
+	{
+		return month ; 
+	}
+	public int getDate()
+	{
+		return date ; 
+	}
+	public void setYear(int ye)
+	{
+		year = ye ; 
+	}
+	public void setMonth(int mon)
+	{
+		month = mon ; 
+	}
+	public void setDate(int da)
+	{
+		date = da ; 
+	}
+		
+}
