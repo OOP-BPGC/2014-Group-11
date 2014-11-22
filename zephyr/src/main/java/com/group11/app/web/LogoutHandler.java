75165c013b672f78029443f7172d2e2860a1e417
R Srinath <f2012560@goa.bits-pilani.ac.in>
2014-11-22 19:18:43 +0530

    Logout functionality completed.

--- zephyr/src/main/java/com/group11/app/web/LogoutHandler.java
+++ zephyr/src/main/java/com/group11/app/web/LogoutHandler.java
@@ -11,7 +11,7 @@ public class LogoutHandler extends HttpServlet {
     
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.getSession().invalidate();
-        response.sendRedirect("/");
+        response.sendRedirect("/zephyr");
         return;
     }
 
